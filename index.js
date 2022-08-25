adfe73a409c97bc01b9251ca0f8040db21488211
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-08-25 14:24:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+const employee = {name:"Elvis Gatukui", streetAddress:"28th Avenue Nairobi"};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, {[key]:value});
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const addObject = Object.assign({}, employee);
+    delete addObject[key];
+    return addObject;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+    return employee;
+}
+
